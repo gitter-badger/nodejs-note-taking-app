de5b7c83a761d7162abd28ef98642742af4888c8
Amrita <amritachaturvedi97@gmail.com>
2019-02-23 17:43:12 +0530

    Added the basic ui

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-router.get('/get/:name', function(req, res){
-    res.send('Hello ' + req.params.name + ' !');
- });
-
-
-
+router.get('/', function(req, res){
+    return res.render('greet', {
+        from: "amrita",
+        to: "amrita"
+    })
+});
 
 //export this router to use in our index.js
 module.exports = router;
